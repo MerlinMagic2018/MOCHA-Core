b9ee33861a237c779aea3c41d7ed16f5d907cc7b
WillWammer <60303987+WillWammer@users.noreply.github.com>
2020-03-14 19:07:26 -0500

    Update chainparams.cpp

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -79,25 +79,25 @@ static const Checkpoints::CCheckpointData dataRegtest = {
     0,
     0};
 
-// libzerocoin::ZerocoinParams* CChainParams::Zerocoin_Params() const
-// {
-//     assert(this);
-//     static CBigNum bnTrustedModulus;
-//     bnTrustedModulus.SetDec(zerocoinModulus);
-//     static libzerocoin::ZerocoinParams ZCParams = libzerocoin::ZerocoinParams(bnTrustedModulus);
+libzerocoin::ZerocoinParams* CChainParams::Zerocoin_Params() const
+{
+    assert(this);
+    static CBigNum bnTrustedModulus;
+    bnTrustedModulus.SetDec(zerocoinModulus);
+    static libzerocoin::ZerocoinParams ZCParams = libzerocoin::ZerocoinParams(bnTrustedModulus);
 
-//     return &ZCParams;
-// }
+    return &ZCParams;
+}
 
-// libzerocoin::ZerocoinParams* CChainParams::OldZerocoin_Params() const
-// {
-//     assert(this);
-//     static CBigNum bnTrustedModulus;
-//     bnTrustedModulus.SetHex(zerocoinModulus);
-//     static libzerocoin::ZerocoinParams ZCParams = libzerocoin::ZerocoinParams(bnTrustedModulus);
+libzerocoin::ZerocoinParams* CChainParams::OldZerocoin_Params() const
+{
+    assert(this);
+    static CBigNum bnTrustedModulus;
+    bnTrustedModulus.SetHex(zerocoinModulus);
+    static libzerocoin::ZerocoinParams ZCParams = libzerocoin::ZerocoinParams(bnTrustedModulus);
 
-//     return &ZCParams;
-// }
+    return &ZCParams;
+}
 
 class CMainParams : public CChainParams
 {
